4204136f5c6873d800fe4a634dc62a034a279a88
RAFA <wicaksonorafha@gmail.com>
2022-10-12 09:04:39 +0700

    intro screen add 2

---
+++ app/src/main/java/id/allana/careerwolu/intro3_activity.kt
@@ -0,0 +1,11 @@
+package id.allana.careerwolu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class intro3_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro3)
+    }
+}
