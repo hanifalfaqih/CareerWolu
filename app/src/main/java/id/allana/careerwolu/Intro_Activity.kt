d044b9ec95004dc38292f05551642fd995f12e44
RAFA <wicaksonorafha@gmail.com>
2022-10-12 07:49:32 +0700

    add color and screen

---
+++ app/src/main/java/id/allana/careerwolu/Intro_Activity.kt
@@ -0,0 +1,11 @@
+package id.allana.careerwolu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Intro_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
---
+++ app/src/main/java/id/allana/careerwolu/StartingScreenActivity.kt
@@ -0,0 +1,11 @@
+package id.allana.careerwolu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartingScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starting_screen)
+    }
+}
